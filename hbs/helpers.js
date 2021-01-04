93ca9c74340b234f6c5194136768ac2a9ccfad6a
Ninfa Niño <ninfa_sys@hotmail.com>
2021-01-03 20:10:24 -0600

    udemy-node-07webserver

---
+++ hbs/helpers.js
@@ -0,0 +1,14 @@
+const hbs = require('hbs')
+    // helpers
+hbs.registerHelper('getYear', () => {
+    return new Date().getFullYear();
+})
+
+hbs.registerHelper('capitalizar', (text) => {
+    let palabras = text.split(' ');
+    palabras.forEach((word, index) => {
+        palabras[index] = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+    });
+
+    return palabras.join(' ');
+})
